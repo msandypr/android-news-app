d769a440c1c5fbdc2cab3f74a50c41a00771a013
msandypr <msandypr@gmail.com>
2024-01-23 23:59:07 +0700

    feat: convert unknown dataType in Source models

---
+++ app/src/main/java/com/msandypr/thesandynews/db/Converters.kt
@@ -0,0 +1,17 @@
+package com.msandypr.thesandynews.db
+
+import androidx.room.TypeConverter
+import com.msandypr.thesandynews.models.Source
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String{
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source{
+        return Source(name, name)
+    }
+}
