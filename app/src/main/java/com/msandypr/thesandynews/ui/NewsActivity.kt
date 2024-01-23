3223a28459bdc22d17556e9b45c454d689aa4e5e
msandypr <msandypr@gmail.com>
2024-01-23 16:51:52 +0700

    feat: setuping layout for development environment

---
+++ app/src/main/java/com/msandypr/thesandynews/ui/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.msandypr.thesandynews.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msandypr.thesandynews.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
