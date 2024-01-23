4e7533282757a717f45ddb805b664dbb036bdc4b
msandypr <msandypr@gmail.com>
2024-01-23 22:56:24 +0700

    feat: models and NewsAPI integration

---
+++ app/src/main/java/com/msandypr/thesandynews/models/NewsResponse.kt
@@ -0,0 +1,7 @@
+package com.msandypr.thesandynews.models
+
+data class NewsResponse(
+    val articles: MutableList<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/msandypr/thesandynews/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.msandypr.thesandynews.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "e64a44e2f37b4eb2af9e054f984a6277"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
