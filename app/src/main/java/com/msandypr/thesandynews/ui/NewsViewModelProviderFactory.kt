4844d0a9f2eb86c1646dd535757539baa84224f5
msandypr <msandypr@gmail.com>
2024-01-24 11:51:43 +0700

    feat: Add NewsViewModelProviderFactory to provide NewsViewModel instances

---
+++ app/src/main/java/com/msandypr/thesandynews/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,12 @@
+package com.msandypr.thesandynews.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.msandypr.thesandynews.repository.NewsRepository
+
+class NewsViewModelProviderFactory(val app: Application, val newsRepository: NewsRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(app, newsRepository) as T
+    }
+}
