2e8b9c02d42e3b0cd8436ec7e1226bd89f872061
msandypr <msandypr@gmail.com>
2024-01-24 11:35:26 +0700

    feat: news view model

--- app/src/main/java/com/msandypr/thesandynews/ui/NewsViewModel.kt
+++ app/src/main/java/com/msandypr/thesandynews/ui/NewsViewModel.kt
@@ -1,12 +1,20 @@
 package com.msandypr.thesandynews.ui
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.msandypr.thesandynews.models.Article
 import com.msandypr.thesandynews.models.NewsResponse
 import com.msandypr.thesandynews.repository.NewsRepository
 import com.msandypr.thesandynews.util.Resource
+import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.io.IOException
+import java.util.Locale.IsoCountryCode
 
 class NewsViewModel(app: Application, val newsRepository: NewsRepository): AndroidViewModel(app) {
 
@@ -20,6 +28,17 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository): Andro
     var newSearchQuery: String? = null
     var oldSearchQuery: String? = null
 
+    init {
+        getHeadlines("id")
+    }
+
+    fun getHeadlines(countryCode: String) = viewModelScope.launch {
+        headlinesInternet(countryCode)
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNewsInternet(searchQuery)
+    }
     private fun handleHeadlinesResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
         if (response.isSuccessful){
             response.body()?.let { resultResponse ->
@@ -35,5 +54,82 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository): Andro
             }
         }
         return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                if (searchNewsResponse == null || newSearchQuery != oldSearchQuery){
+                    searchNewsPage = 1
+                    oldSearchQuery = newSearchQuery
+                    searchNewsResponse = resultResponse
+                } else {
+                    searchNewsPage++
+                    val oldArticles = searchNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun addToBookmarks(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getBookmarkNews() = newsRepository.getBookmarkNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+    fun internetConnection(context: Context) : Boolean {
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run {
+                when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        }
+    }
+
+    private suspend fun headlinesInternet(countryCode: String) {
+        headlines.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())) {
+                val response = newsRepository.getHeadlines(countryCode, headlinesPage)
+                headlines.postValue(handleHeadlinesResponse(response))
+            } else {
+                headlines.postValue(Resource.Error("No Internet Connection"))
+            }
+        } catch (t: Throwable) {
+            when(t) {
+                is IOException -> headlines.postValue(Resource.Error("Unable to Connect the Internet"))
+                else -> headlines.postValue(Resource.Error("No Signal"))
+            }
+        }
+    }
+
+    private suspend fun searchNewsInternet(searchQuery: String) {
+        newSearchQuery = searchQuery
+        searchNews.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())) {
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleSearchNewsResponse(response))
+            } else {
+                searchNews.postValue(Resource.Error("No Internet Connection"))
+            }
+        } catch (t: Throwable) {
+            when(t) {
+                is IOException -> searchNews.postValue(Resource.Error("Unable to Connect the Internet"))
+                else -> searchNews.postValue(Resource.Error("No Signal"))
+            }
+        }
     }
 }
