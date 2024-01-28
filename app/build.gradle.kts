824ed207e319956a09d5a6b837a5375c4ad13b95
msandypr <msandypr@gmail.com>
2024-01-29 00:26:22 +0700

    feat: development environment for qr code scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     ksp ("com.github.bumptech.glide:compiler:4.12.0")
 
+    //ML Kit Barcode
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+
 }
