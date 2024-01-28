17a32d6c47b303da878691fa107f8ccae52e16e3
msandypr <msandypr@gmail.com>
2024-01-28 14:56:38 +0700

    feat: Implement SearchFragment with dynamic search, RecyclerView, and error handling

--- app/src/main/java/com/msandypr/thesandynews/ui/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/msandypr/thesandynews/ui/fragments/BookmarkFragment.kt
@@ -2,22 +2,77 @@ package com.msandypr.thesandynews.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.msandypr.thesandynews.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.msandypr.thesandynews.adapters.NewsAdapter
+import com.msandypr.thesandynews.databinding.FragmentBookmarkBinding
+import com.msandypr.thesandynews.ui.NewsActivity
+import com.msandypr.thesandynews.ui.NewsViewModel
 
 class BookmarkFragment : Fragment() {
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var binding: FragmentBookmarkBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBookmarkBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setupBookmarkRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(R.id.action_bookmarkFragment_to_articleFragment, bundle)
+        }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                newsViewModel.deleteArticle(article)
+                Snackbar.make(view, "Removed from Bookmark", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo"){
+                        newsViewModel.addToBookmarks(article)
+                    }
+                    show()
+                }
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.recyclerBookmark)
+        }
+
+        newsViewModel.getBookmarkNews().observe(viewLifecycleOwner, Observer { articles ->
+            newsAdapter.differ.submitList(articles)
+        })
     }
 
+    private fun setupBookmarkRecycler() {
+        newsAdapter = NewsAdapter()
+        binding.recyclerBookmark.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 }
