c651a8b08ed461ac7bf8a89a54788236278eaa55
msandypr <msandypr@gmail.com>
2024-01-28 10:17:28 +0700

    feat: Enhance HeadlineFragment with RecyclerView, pagination, and error handling

--- app/src/main/java/com/msandypr/thesandynews/ui/fragments/HeadlineFragment.kt
+++ app/src/main/java/com/msandypr/thesandynews/ui/fragments/HeadlineFragment.kt
@@ -2,22 +2,75 @@ package com.msandypr.thesandynews.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.msandypr.thesandynews.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.msandypr.thesandynews.adapters.NewsAdapter
+import com.msandypr.thesandynews.databinding.FragmentHeadlineBinding
+import com.msandypr.thesandynews.ui.NewsViewModel
 class HeadlineFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headline, container, false)
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+    lateinit var binding: FragmentHeadlineBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlineBinding.bind(view)
+    }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar(){
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = true
+    }
+
+    private fun hideErrorMessage(){
+        itemHeadlinesError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String){
+        itemHeadlinesError.visibility = View.INVISIBLE
+        errorText.text = message
+        isError = true
+    }
+
+    var scrollListener = object : RecyclerView.OnScrollListener(){
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoErrors = !isError
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= com.msandypr.thesandynews.util.Constants.QUERY_PAGE_SIZE
+            val shouldPaginate = isNoErrors && isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate){
+                newsViewModel.getHeadlines("id")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+        }
     }
 
 }
